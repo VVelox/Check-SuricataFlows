26a9476d42847322b04f3d81a58df34f8623d1d6
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2026-02-11 14:36:11 -0600

    ready for testing

--- lib/Check/SuricataFlows.pm
+++ lib/Check/SuricataFlows.pm
@@ -3,19 +3,22 @@ package Check::SuricataFlows;
 use 5.006;
 use strict;
 use warnings;
+use File::ReadBackwards ();
+use JSON                qw(decode_json);
+use Scalar::Util        qw(looks_like_number);
+use Time::Piece         qw( localtime );
 
 =head1 NAME
 
-Check::SuricataFlows - The great new Check::SuricataFlows!
+Check::SuricataFlows - Make sure Suricata is seeing data via reading the Suricata flows json
 
 =head1 VERSION
 
-Version 0.01
+Version 0.0.1
 
 =cut
 
-our $VERSION = '0.01';
-
+our $VERSION = '0.0.1';
 
 =head1 SYNOPSIS
 
@@ -28,26 +31,225 @@ Perhaps a little code snippet.
     my $foo = Check::SuricataFlows->new();
     ...
 
-=head1 EXPORT
+=head1 METHODS
 
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
+=head2 new
 
-=head1 SUBROUTINES/METHODS
+Initiates the object.
 
-=head2 function1
+    - max_lines :: Maximum distance to read back. The sizing of this should be large enough to
+           ensure you get enought data that has bidirectional flow info. Likely need to increase for
+           very noisy networks.
+        default :: 500
+
+    - read_back_time :: How far back to read in seconds. Set to 0 to disable.
+        default :: 300
+
+    - warn_count :: Warn if it is less then this for bidirectional traffic.
+        Note :: This value was arbitrarrily choosen. Adjust for your enviroment.
+        default :: 10
+
+    - alert_count :: Alert if it is less than this for bidirectional traffic.
+        Note :: This value was arbitrarrily choosen. Adjust for your enviroment.
+        default :: 1
+
+    - flow_file :: The location json file containing the flow data.
+          default :: /var/log/suricata/flows/current/flow.json
 
 =cut
 
-sub function1 {
-}
+sub new {
+	my ( $blank, %opts ) = @_;
 
-=head2 function2
+	if ( !defined( $opts{max_lines} ) ) {
+		$opts{max_lines} = 500;
+	} elsif ( ref( $opts{max_lines} ) ne '' ) {
+		die( '$opts{max_lines} of ref type "' . ref( $opts{max_lines} ) . '" and not ""' );
+	} elsif ( !looks_like_number( $opts{max_lines} ) ) {
+		die( '$opts{max_lines}, "' . $opts{max_lines} . '", does not appear to be numeric' );
+	} elsif ( $opts{max_lines} < 1 ) {
+		die( '$opts{max_lines}, "' . $opts{max_lines} . '", must be 1 or greater' );
+	}
+
+	if ( !defined( $opts{read_back_time} ) ) {
+		$opts{read_back_time} = 300;
+	} elsif ( ref( $opts{read_back_time} ) ne '' ) {
+		die( '$opts{read_back_time} of ref type "' . ref( $opts{read_back_time} ) . '" and not ""' );
+	} elsif ( !looks_like_number( $opts{read_back_time} ) ) {
+		die( '$opts{read_back_time}, "' . $opts{read_back_time} . '", does not appear to be numeric' );
+	} elsif ( $opts{read_back_time} < 1 ) {
+		die( '$opts{read_back_time}, "' . $opts{read_back_time} . '", must be 1 or greater' );
+	}
+
+	if ( !defined( $opts{warn_count} ) ) {
+		$opts{warn_count} = 10;
+	} elsif ( ref( $opts{warn_count} ) ne '' ) {
+		die( '$opts{warn_count} of ref type "' . ref( $opts{warn_count} ) . '" and not ""' );
+	} elsif ( !looks_like_number( $opts{warn_count} ) ) {
+		die( '$opts{warn_count}, "' . $opts{warn_count} . '", does not appear to be numeric' );
+	}
+
+	if ( !defined( $opts{alert_count} ) ) {
+		$opts{alert_count} = 500;
+	} elsif ( ref( $opts{alert_count} ) ne '' ) {
+		die( '$opts{alert_count} of ref type "' . ref( $opts{alert_count} ) . '" and not ""' );
+	} elsif ( !looks_like_number( $opts{alert_count} ) ) {
+		die( '$opts{alert_count}, "' . $opts{alert_count} . '", does not appear to be numeric' );
+	}
+
+	if ( !defined( $opts{flow_file} ) ) {
+		$opts{flow_file} = '/var/log/suricata/flows/current/flow.json';
+	} elsif ( ref( $opts{flow_file} ) ne '' ) {
+		die( '$opts{flow_file} of ref type "' . ref( $opts{flow_file} ) . '" and not ""' );
+	}
+
+	my $self = {
+		flow_file      => $opts{flow_file},
+		alert_count    => $opts{alert_count},
+		warn_count     => $opts{warn_count},
+		read_back_time => $opts{read_back_time},
+		max_lines      => $opts{max_lines},
+	};
+	bless $self;
+
+	return $self;
+} ## end sub new
+
+=head2 run
 
 =cut
 
-sub function2 {
-}
+sub run {
+	my $self = $_[0];
+
+	my $to_return = {
+		status                => '',
+		status_code           => 0,
+		lines_read            => 0,
+		lines_parsed          => 0,
+		lines_get_errored     => 0,
+		bi_directional_count  => 0,
+		uni_directional_count => 0,
+		lines_get_errors      => [],
+	};
+
+	# can't go any further if we can't read the file
+	if ( !-e $self->{flow_file} ) {
+		$to_return->{status}      = "flow file, '" . $self->{flow_file} . "', does not exist\n";
+		$to_return->{status_code} = 3;
+		return $to_return;
+	} elsif ( !-r $self->{flow_file} ) {
+		$to_return->{status}      = "flow file, '" . $self->{flow_file} . "', not readable\n";
+		$to_return->{status_code} = 3;
+		return $to_return;
+	}
+
+	# get the current time and figure when we should read back till
+	my $read_till = localtime;
+	$read_till = $read_till - $self->{read_back_time};
+
+	# init readbackwards and if we can't we can't go any further
+	my $bw;
+	eval { $bw = File::ReadBackwards->new( $self->{flow_file} )
+			or die "can't read '" . $self->{flow_file} . "' $!"; };
+	if ($@) {
+		$to_return->{status}
+			= "Failed to initiate File::ReadBackwards for  '" . $self->{flow_file} . "' ... " . $@ . "\n";
+		$to_return->{status_code} = 3;
+		return $to_return;
+	}
+
+	# as long as this is true, we should continue reading the file
+	my $process = 1;
+	# for keeping track of number of lines read
+	my $line_count;
+	while ($process) {
+		$line_count++;
+		if ( $line_count > $self->{max_lines} ) {
+			$process = 0;
+		} elsif ( $bw->eof ) {
+			$process = 0;
+		}
+
+		if ($process) {
+			my $parsed_line;
+			my $process_line = 0;
+			eval {
+				my $line = $bw->readline;
+
+				$to_return->{lines_read}++;
+
+				$parsed_line = decode_json($line);
+
+				$to_return->{lines_parsed}++;
+
+				$process_line = 1;
+			};
+			if ($@) {
+				push( @{ $to_return->{lines_get_errors} }, $@ );
+				$to_return->{lines_get_errored}++;
+			} elsif ($process_line) {
+				my $time_good = 0;
+
+				if ( defined( $parsed_line->{'timestamp'} ) ) {
+					$parsed_line->{'timestamp'} =~ s/\.[0-9]+//;
+
+					my $time;
+					eval { $time = Time::Piece->strptime( $parsed_line->{'timestamp'}, '%Y-%m-%dT%T%z' ); };
+					if ( defined($time) && $time >= $read_till ) {
+						$time_good = 1;
+					} elsif ( defined($time) && $time < $read_till ) {
+						$process = 0;
+					}
+				} ## end if ( defined( $parsed_line->{'timestamp'} ...))
+
+				if (   $time_good
+					&& defined( $parsed_line->{'dest_ip'} )
+					&& ( ref( $parsed_line->{'dest_ip'} ) eq '' )
+					&& defined( $parsed_line->{'src_ip'} )
+					&& ( ref( $parsed_line->{'src_ip'} ) eq '' )
+					&& defined( $parsed_line->{'flow'} )
+					&& ( ref( $parsed_line->{'flow'} ) eq 'HASH' )
+					&& defined( $parsed_line->{'flow'}{'pkts_toclient'} )
+					&& ( ref( $parsed_line->{'flow'}{'pkts_toclient'} ) eq '' )
+					&& looks_like_number( $parsed_line->{'flow'}{'pkts_toclient'} )
+					&& defined( $parsed_line->{'flow'}{'pkts_toserver'} )
+					&& ( ref( $parsed_line->{'flow'}{'pkts_toserver'} ) eq '' )
+					&& looks_like_number( $parsed_line->{'flow'}{'pkts_toserver'} ) )
+				{
+					if (   ( $parsed_line->{'flow'}{'pkts_toserver'} > 0 )
+						&& ( $parsed_line->{'flow'}{'pkts_toclient'} > 0 ) )
+					{
+						$to_return->{bi_directional_count}++;
+					} else {
+						$to_return->{uni_directional_count}++;
+					}
+				} ## end if ( $time_good && defined( $parsed_line->...))
+			} ## end elsif ($process_line)
+		} ## end if ($process)
+	} ## end while ($process)
+
+	if ( $to_return->{bi_directional_count} <= $self->{alert_count} ) {
+		$to_return->{status}
+			= 'bi directional count of '
+			. $to_return->{bi_directional_count}
+			. ' is less than '
+			. $self->{alert_count} . "\n";
+		$to_return->{status_code} = 2;
+	} elsif ( $to_return->{bi_directional_count} <= $self->{warn_count} ) {
+		$to_return->{status_code} = 1;
+	} else {
+		$to_return->{status_code} = 0;
+	}
+
+	$to_return->{status} = $to_return->{status} . 'uni directional: ' . $to_return->{uni_directional_count} . "\n";
+	$to_return->{status} = $to_return->{status} . 'max lines to read: ' . $self->{max_lines} . "\n";
+	$to_return->{status} = $to_return->{status} . 'lines read: ' . $to_return->{lines_read} . "\n";
+	$to_return->{status} = $to_return->{status} . 'lines parsed: ' . $to_return->{lines_parsed} . "\n";
+	$to_return->{status} = $to_return->{status} . 'line gets errored: ' . $to_return->{lines_get_errored} . "\n";
+
+	return $to_return;
+} ## end sub run
 
 =head1 AUTHOR
 
@@ -102,4 +304,4 @@ This is free software, licensed under:
 
 =cut
 
-1; # End of Check::SuricataFlows
+1;    # End of Check::SuricataFlows
